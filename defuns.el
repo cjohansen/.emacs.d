420bd7853be25cc7a231edc51aa05471ba04a38a
Christian Johansen <christian.johansen@finn.no>
2011-10-13 08:13:58 +0200

    Don't indent when making newline on the end of the line

--- defuns.el
+++ defuns.el
@@ -192,9 +192,11 @@ Symbols matching the text at point are put first in the completion list."
 
 (defun new-line-below ()
   (interactive)
-  (end-of-line)
-  (newline)
-  (indent-for-tab-command))
+  (if (eolp)
+      (newline)
+    (end-of-line)
+    (newline)
+    (indent-for-tab-command)))
 
 (defun new-line-in-between ()
   (interactive)
