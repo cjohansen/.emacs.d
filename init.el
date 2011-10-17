f104db86d45c5725d5a02e972436b20354afcbed
Christian Johansen <christian.johansen@finn.no>
2011-10-17 07:51:44 +0200

    Disable half-baked ruby eval-line

--- init.el
+++ init.el
@@ -125,7 +125,7 @@
 (add-to-list 'load-path "~/.emacs.d/site-lisp/yaml-mode")
 (add-to-list 'load-path "~/.emacs.d/site-lisp/emms/lisp")
 (add-to-list 'load-path "~/.emacs.d/site-lisp/recall-position")
-(add-to-list 'load-path "~/.emacs.d/site-lisp/eval-lines")
+;;(add-to-list 'load-path "~/.emacs.d/site-lisp/eval-lines")
 (add-to-list 'load-path "~/.emacs.d/site-lisp/magit")
 
 ;; Additional configuration
