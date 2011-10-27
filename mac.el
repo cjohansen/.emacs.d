610f05f7d2054108bf7f103e9c11e950da17309c
Magnar Sveen <magnars@gmail.com>
2011-10-27 07:37:56 +0200

    Better mac-integration:   - Make ido-mode ignore .DS_Store   - Move deleted files to trash

--- mac.el
+++ mac.el
@@ -12,4 +12,11 @@
 ;; keybinding to toggle full screen mode
 (global-set-key (quote [M-f10]) (quote ns-toggle-fullscreen))
 
+;; Move to trash when deleting stuff
+(setq delete-by-moving-to-trash t
+      trash-directory "~/.Trash/emacs")
+
+;; Ignore .DS_Store files with ido mode
+(add-to-list 'ido-ignore-files "\\.DS_Store")
+
 (provide 'mac)
