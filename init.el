b459b48a109d58835b697229ab48bf6a57c126ef
Magnar Sveen <magnars@gmail.com>
2011-10-31 19:24:22 +0100

    Hippie-mode expands from buffer before files.

--- init.el
+++ init.el
@@ -57,9 +57,15 @@
 ;; Seed the random-number generator
 (random t)
 
-;; Hippie expand: at times perhaps too hip
-(delete 'try-expand-line hippie-expand-try-functions-list)
-(delete 'try-expand-list hippie-expand-try-functions-list)
+;; Hippie expand: look in buffer before filenames please
+(setq hippie-expand-try-functions-list '(try-expand-dabbrev
+                                         try-expand-dabbrev-all-buffers
+                                         try-expand-dabbrev-from-kill
+                                         try-complete-file-name-partially
+                                         try-complete-file-name
+                                         try-expand-all-abbrevs
+                                         try-complete-lisp-symbol-partially
+                                         try-complete-lisp-symbol))
 
 ;; Misc
 (set-default 'indent-tabs-mode nil)
