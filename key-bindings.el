c93b7cf3e901e3464561598d73ee6954440ae677
Magnar Sveen <magnars@gmail.com>
2011-11-10 19:22:29 +0100

    Don't kill emacs so easily please  - New binding is C-x r q  - the mnemonic is C-x REALLY QUIT  - C-x C-c now just kills the frame

--- key-bindings.el
+++ key-bindings.el
@@ -1,3 +1,8 @@
+;; I don't need to kill emacs that easily
+;; the mnemonic is C-x REALLY QUIT
+(global-set-key (kbd "C-x r q") 'save-buffers-kill-terminal)
+(global-set-key (kbd "C-x C-c") 'delete-frame)
+
 ;; Completion that uses many different methods to find options.
 (global-set-key (kbd "C-.") 'hippie-expand)
 
