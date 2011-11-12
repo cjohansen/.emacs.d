5f0a8802eed23939c3bd73a3ad4ce3a829bb2e2e
Magnar Sveen <magnars@gmail.com>
2011-11-12 13:50:51 +0100

    Formatting.

--- defuns/buffer-defuns.el
+++ defuns/buffer-defuns.el
@@ -18,18 +18,20 @@
     ))
 
 (defun swap-windows ()
- "If you have 2 windows, it swaps them." (interactive) (cond ((not (= (count-windows) 2)) (message "You need exactly 2 windows to do this."))
- (t
- (let* ((w1 (first (window-list)))
-	 (w2 (second (window-list)))
-	 (b1 (window-buffer w1))
-	 (b2 (window-buffer w2))
-	 (s1 (window-start w1))
-	 (s2 (window-start w2)))
- (set-window-buffer w1 b2)
- (set-window-buffer w2 b1)
- (set-window-start w1 s2)
- (set-window-start w2 s1)))))
+  "If you have 2 windows, it swaps them."
+  (interactive)
+  (cond ((not (= (count-windows) 2)) (message "You need exactly 2 windows to do this."))
+        (t
+         (let* ((w1 (first (window-list)))
+                (w2 (second (window-list)))
+                (b1 (window-buffer w1))
+                (b2 (window-buffer w2))
+                (s1 (window-start w1))
+                (s2 (window-start w2)))
+           (set-window-buffer w1 b2)
+           (set-window-buffer w2 b1)
+           (set-window-start w1 s2)
+           (set-window-start w2 s1)))))
 
 (defun ido-imenu ()
   "Update the imenu index and then use ido to select a symbol to navigate to.
