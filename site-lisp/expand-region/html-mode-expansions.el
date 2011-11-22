354bdb6068686c79a2f151dcb49037b4cb9e79c8
Magnar Sveen <magnars@gmail.com>
2011-11-22 16:10:21 +0100

    Added mark-inner/outer-tag to html-mode-expansions

--- site-lisp/expand-region/html-mode-expansions.el
+++ site-lisp/expand-region/html-mode-expansions.el
@@ -1,4 +1,7 @@
 (defun er/mark-html-attribute ()
+  "Mark html-attribute presumes that point is at the assignment part of attr=\"value\".
+If point is inside the value-string, the quotes will be marked first anyway.
+Does not support html-attributes with spaces around the equal sign or unquotes attributes atm."
   (interactive)
   (when (or (looking-at "\\(\\s_\\|\\sw\\)*=")
             (looking-back "="))
@@ -10,11 +13,30 @@
     (er--move-point-forward-out-of-string)
     (exchange-point-and-mark)))
 
+(defun er/mark-outer-tag ()
+  (interactive)
+  (sgml-get-context)
+  (set-mark (point))
+  (sgml-skip-tag-forward 1)
+  (exchange-point-and-mark))
+
+(defun er/mark-inner-tag ()
+  (interactive)
+  (goto-char (aref (nth 0 (sgml-get-context)) 3))
+  (set-mark (point))
+  (backward-char 1)
+  (sgml-skip-tag-forward 1)
+  (search-backward "</")
+  (exchange-point-and-mark))
+
+
 (defun er/add-html-mode-expansions ()
   (make-variable-buffer-local 'er/try-expand-list)
   (setq er/try-expand-list (append
                             er/try-expand-list
-                            '(er/mark-html-attribute))))
+                            '(er/mark-html-attribute
+                              er/mark-inner-tag
+                              er/mark-outer-tag))))
 
 (add-hook 'html-mode-hook 'er/add-html-mode-expansions)
 
