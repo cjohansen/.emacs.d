1b36a1019ba7655b4403558807a0325891e95e16
Magnar Sveen <magnars@gmail.com>
2011-11-25 21:39:49 +0100

    Don't use js2-mode for JSON.

--- mode-mappings.el
+++ mode-mappings.el
@@ -40,7 +40,7 @@
 ;; JavaScript
 (autoload 'js2-mode "setup-js2-mode")
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
-(add-to-list 'auto-mode-alist '("\\.json$" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.json$" . javascript-mode))
 (add-to-list 'magic-mode-alist '("#!/usr/bin/env node" . js2-mode))
 
 ;; Snippets
