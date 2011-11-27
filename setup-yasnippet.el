06ec25e13751dd6e7c4ad5a82fd38f257afb2df3
Magnar Sveen <magnars@gmail.com>
2011-11-27 12:27:26 +0100

    Move my snippets to own folder.

--- setup-yasnippet.el
+++ setup-yasnippet.el
@@ -1,8 +1,15 @@
 ;; Load and initialize yasnippet
 (require 'yasnippet)
 (yas/initialize)
-(yas/load-directory (expand-file-name "yasnippet/snippets" site-lisp-dir))
 
+;; Develop in ~/emacs.d/snippets, but also
+;; include snippets that come with yasnippet
+(setq yas/root-directory `(,(expand-file-name "snippets" dotfiles-dir)
+                           ,(expand-file-name "yasnippet/snippets" site-lisp-dir)))
+
+(mapc 'yas/load-directory yas/root-directory)
+
+;; Include snippets for Buster.js
 (require 'buster-snippets)
 
 ;; No dropdowns please, yas
