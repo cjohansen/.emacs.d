36142bacca0e2247156012bfdc7b01d09107a836
Magnar Sveen <magnars@gmail.com>
2011-12-01 08:20:22 +0100

    Move iy- to fj/fh  - two fingers on home row  - also, I need my asdf

--- key-chords.el
+++ key-chords.el
@@ -1,9 +1,9 @@
 (require 'key-chord)
 (key-chord-mode 1)
 
-;; Move to char similar to "f" in vim, f+g forward, d+f backward
-(key-chord-define-global "fg" 'iy-go-to-char)
-(key-chord-define-global "df" 'iy-go-to-char-backward)
+;; Move to char similar to "f" in vim, f+j forward, f+h backward
+(key-chord-define-global "fj" 'iy-go-to-char)
+(key-chord-define-global "fh" 'iy-go-to-char-backward)
 
 ;; Indent entire buffer and fix whitespace
 (key-chord-define-global "i0" 'indent-buffer)
