df1e7fb0b931f8cb654e23188829554fecc4dc10
Magnar Sveen <magnars@gmail.com>
2011-12-05 07:31:42 +0100

    Don't show Undo-Tree lighter.

--- sane-defaults.el
+++ sane-defaults.el
@@ -68,6 +68,7 @@
 (require 'smooth-scrolling)
 
 ;; Represent undo-history as an actual tree (visualize with C-x u)
+(setq undo-tree-mode-lighter "")
 (require 'undo-tree)
 (global-undo-tree-mode)
 
