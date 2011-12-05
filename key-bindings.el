8d64fc31ef9b2bb31e6602033eafa2eaa8e9942c
Magnar Sveen <magnars@gmail.com>
2011-12-05 06:07:24 +0100

    Make C-h true DEL.

--- key-bindings.el
+++ key-bindings.el
@@ -30,8 +30,7 @@
 (global-set-key (kbd "C-<f10>") 'menu-bar-mode)
 
 ;; Use shell-like backspace C-h, rebind help to F1
-(global-set-key (kbd "C-h") 'backward-delete-char-untabify)
-(define-key isearch-mode-map (kbd "C-h") 'isearch-delete-char)
+(define-key key-translation-map [?\C-h] [?\C-?])
 (global-set-key (kbd "<f1>") 'help-command)
 
 ;; Killing text
