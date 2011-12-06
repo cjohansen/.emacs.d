0b8fd33dd1fa96965edc6cc69565d7f70ab9e5fd
Magnar Sveen <magnars@gmail.com>
2011-12-06 22:40:18 +0100

    Shortcut to exit snippet quickly. C-<return>

--- setup-yasnippet.el
+++ setup-yasnippet.el
@@ -12,6 +12,9 @@
 ;; Include snippets for Buster.js
 (require 'buster-snippets)
 
+;; Jump to end of snippet definition
+(define-key yas/keymap (kbd "C-<return>") 'yas/exit-all-snippets)
+
 ;; No dropdowns please, yas
 (setq yas/prompt-functions '(yas/ido-prompt yas/completing-prompt))
 
