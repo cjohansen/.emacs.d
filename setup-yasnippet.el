f7ce3578d6b389d3610fdfe58ec5b0e259401375
Magnar Sveen <magnars@gmail.com>
2011-12-07 06:33:37 +0100

    Inter-field navigation in yasnippets.

--- setup-yasnippet.el
+++ setup-yasnippet.el
@@ -15,6 +15,26 @@
 ;; Jump to end of snippet definition
 (define-key yas/keymap (kbd "C-<return>") 'yas/exit-all-snippets)
 
+;; Inter-field navigation
+(defun yas/goto-end-of-active-field ()
+  (interactive)
+  (let* ((snippet (car (yas/snippets-at-point)))
+        (position (yas/field-end (yas/snippet-active-field snippet))))
+    (if (= (point) position)
+        (move-end-of-line 1)
+      (goto-char position))))
+
+(defun yas/goto-start-of-active-field ()
+  (interactive)
+  (let* ((snippet (car (yas/snippets-at-point)))
+        (position (yas/field-start (yas/snippet-active-field snippet))))
+    (if (= (point) position)
+        (move-beginning-of-line 1)
+      (goto-char position))))
+
+(define-key yas/keymap (kbd "C-e") 'yas/goto-end-of-active-field)
+(define-key yas/keymap (kbd "C-a") 'yas/goto-start-of-active-field)
+
 ;; No dropdowns please, yas
 (setq yas/prompt-functions '(yas/ido-prompt yas/completing-prompt))
 
