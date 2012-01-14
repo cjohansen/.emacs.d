96bed724f8e05a486b56235b89e11eb2b52fec90
Magnar Sveen <magnars@gmail.com>
2012-01-14 13:03:21 +0100

    More utf-8.

--- sane-defaults.el
+++ sane-defaults.el
@@ -29,8 +29,10 @@
 (defalias 'yes-or-no-p 'y-or-n-p)
 
 ;; UTF-8 please
+(setq locale-coding-system 'utf-8)
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
 (prefer-coding-system 'utf-8)
 
 ;; Show active region
