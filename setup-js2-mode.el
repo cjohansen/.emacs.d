a86ccb9751a8b4c014678c8dede613a7fcdaf33b
Magnar Sveen <magnars@gmail.com>
2012-01-20 14:39:51 +0100

    Register browser API as js2-externs.

--- setup-js2-mode.el
+++ setup-js2-mode.el
@@ -2,7 +2,7 @@
 (setq-default js2-auto-indent-p nil)
 (setq-default js2-basic-offset 2)
 (setq-default js2-enter-indents-newline nil)
-(setq-default js2-global-externs '("module" "require" "jQuery" "$" "_" "buster" "sinon" "assert" "refute"))
+(setq-default js2-global-externs '("module" "require" "jQuery" "$" "_" "buster" "sinon" "assert" "refute" "setTimeout" "clearTimeout" "location" "setInterval"))
 (setq-default js2-idle-timer-delay 0.1)
 (setq-default js2-indent-on-enter-key nil)
 (setq-default js2-mirror-mode nil)
