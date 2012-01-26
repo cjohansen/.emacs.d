01755a0ac9595bea39881057fed364e935316dce
Magnar Sveen <magnars@gmail.com>
2012-01-26 15:41:40 +0100

    Organized project-mappings.

--- users/fimasvee/project-mappings.el
+++ users/fimasvee/project-mappings.el
@@ -1,11 +1,27 @@
-;; Project peculiarities: FINN Oppdrag
+;; FINN Oppdrag
+
+(defun custom-persp/oppdrag ()
+  (interactive)
+  (custom-persp "oppdrag"
+                (find-file "~/projects/finn-oppdrag/oppdrag-services/app-main/web/src/")))
+
+(define-key persp-mode-map (kbd "C-x p o") 'custom-persp/oppdrag)
+
 (require 'oppdrag-mode)
 (add-hook 'find-file-hook
           (lambda ()
             (when (string-match-p "oppdrag-services" (buffer-file-name))
               (oppdrag-mode))))
 
-;; Project peculiarities: Zombie TDD
+;; Zombie TDD
+
+(defun custom-persp/zombie ()
+  (interactive)
+  (custom-persp "zombie"
+                (find-file "~/projects/zombietdd/")))
+
+(define-key persp-mode-map (kbd "C-x p z") 'custom-persp/zombie)
+
 (add-hook 'js2-mode-hook
           (lambda ()
             (when (string-match-p "zombietdd" (buffer-file-name))
@@ -14,35 +30,27 @@
               (setq buster-add-default-global-to-iife t)
               (setq buster-use-strict t))))
 
-;; Perspectives
-
-(defun custom-persp/oppdrag ()
-  (interactive)
-  (custom-persp "oppdrag"
-                (find-file "~/projects/finn-oppdrag/oppdrag-services/app-main/web/src/")))
-
-(defun custom-persp/zombie ()
-  (interactive)
-  (custom-persp "zombie"
-                (find-file "~/projects/zombietdd/")))
+;; Adventur
 
 (defun custom-persp/adventur ()
   (interactive)
   (custom-persp "adventur"
                 (find-file "~/projects/adventur/nettsidene/adventur_no/source/backlog.txt")))
 
+(define-key persp-mode-map (kbd "C-x p a") 'custom-persp/adventur)
+
+;; Emacs
+
 (defun custom-persp/emacs ()
   (interactive)
   (custom-persp "emacs"
                 (find-file "~/.emacs.d/init.el")))
+(define-key persp-mode-map (kbd "C-x p e") 'custom-persp/emacs)
+
+;; Org
 
 (defun custom-persp/org ()
   (interactive)
   (custom-persp "org"
                 (find-file "~/Dropbox/org/")))
-
-(define-key persp-mode-map (kbd "C-x p o") 'custom-persp/oppdrag)
-(define-key persp-mode-map (kbd "C-x p z") 'custom-persp/zombie)
-(define-key persp-mode-map (kbd "C-x p a") 'custom-persp/adventur)
-(define-key persp-mode-map (kbd "C-x p e") 'custom-persp/emacs)
 (define-key persp-mode-map (kbd "C-<f6>") 'custom-persp/org)
