0778415abed0c6c8ccff1815d7c62312e32f9741
Magnar Sveen <magnars@gmail.com>
2012-02-11 11:54:13 +0100

    Wrap region: l for <li></li> in html-mode

--- setup-wrap-region.el
+++ setup-wrap-region.el
@@ -12,5 +12,6 @@
 
 (wrap-region-add-wrapper "<c:out value=\"" "\"/>" "o" 'html-mode)
 (wrap-region-add-wrapper "<p>" "</p>" "p" 'html-mode)
+(wrap-region-add-wrapper "<li>" "</li>" "l" 'html-mode)
 
 (provide 'setup-wrap-region)
