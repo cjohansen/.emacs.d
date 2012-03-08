6bb8e4dfda530372a8768c001fb78af85baa1955
Magnar Sveen <magnars@gmail.com>
2012-03-08 06:22:56 +0100

    Add cull.js to personal project mappings.

--- users/fimasvee/project-mappings.el
+++ users/fimasvee/project-mappings.el
@@ -25,10 +25,30 @@
 (add-hook 'js2-mode-hook
           (lambda ()
             (when (string-match-p "zombietdd" (buffer-file-name))
-              (setq js2-additional-externs '("ZOMBIE" "Faye" "EventEmitter"))
+              (setq js2-additional-externs '("ZOMBIE" "Faye" "EventEmitter" "when"))
               (setq buster-default-global "ZOMBIE")
               (setq buster-add-default-global-to-iife t)
               (setq buster-use-strict t)
+              (setq js2r-use-strict t))))
+
+;; culljs
+
+(defun custom-persp/culljs ()
+  (interactive)
+  (custom-persp "culljs"
+                (find-file "~/projects/culljs/")))
+
+(define-key persp-mode-map (kbd "C-x p c") 'custom-persp/culljs)
+
+(add-hook 'js2-mode-hook
+          (lambda ()
+            (when (string-match-p "projects/culljs" (buffer-file-name))
+              (setq buster-default-global "cull")
+              (setq buster-add-default-global-to-iife t)
+              (setq buster-use-strict t)
+              (make-variable-buffer-local 'buster-test-prefix)
+              (setq buster-test-prefix "")
+              (set (make-local-variable 'js2-basic-offset) 4)
               (setq js2r-use-strict t))))
 
 ;; Adventur
