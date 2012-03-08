247e5317d5af339daa5162047adfe4531898fdf3
Magnar Sveen <magnars@gmail.com>
2012-03-08 06:26:04 +0100

    Enable mouse wheel scrolling by default.

--- users/fimasvee/init.el
+++ users/fimasvee/init.el
@@ -1,4 +1,6 @@
-;; stuff goes here yo
-
+;; Default js indentation levels
 (setq-default js2-basic-offset 2)
 (setq js-indent-level 2)
+
+;; Disallow scrolling with mouse wheel
+(mouse-wheel-mode nil)
