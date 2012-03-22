595e56f2257419bcd681cd77ce920300935d9ca4
Magnar Sveen <magnars@gmail.com>
2012-03-22 16:17:16 +0100

    Personal: Use ffip-settings also in dired buffers.

--- users/fimasvee/project-mappings.el
+++ users/fimasvee/project-mappings.el
@@ -1,8 +1,13 @@
 (defmacro project-specifics (name &rest body)
-  `(add-hook 'find-file-hook
+  `(progn
+     (add-hook 'find-file-hook
              (lambda ()
                (when (string-match-p ,name (buffer-file-name))
-                 ,@body))))
+                 ,@body)))
+     (add-hook 'dired-after-readin-hook
+             (lambda ()
+               (when (string-match-p ,name (dired-current-directory))
+                 ,@body)))))
 
 ;; FINN Oppdrag
 
