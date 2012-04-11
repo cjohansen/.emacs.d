b7f5413ca986bbbd6fa60f30f352a3871b2da156
Magnar Sveen <magnars@gmail.com>
2012-04-11 20:12:23 +0200

    Change ido-imenu to use push-mark instead of advice.
     - Don't really need to advice a function that's defined in my defuns-directory.

--- key-bindings.el
+++ key-bindings.el
@@ -82,7 +82,7 @@
 (global-set-key (kbd "<C-tab>") 'bury-buffer)
 
 ;; Jump to a definition in the current file. (This is awesome.)
-(global-set-key (kbd "C-x C-i") 'ido-imenu-push-mark)
+(global-set-key (kbd "C-x C-i") 'ido-imenu)
 
 ;; File finding
 (global-set-key (kbd "C-x M-f") 'ido-find-file-other-window)
