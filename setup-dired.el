fae4722b06cc75f241ac8606ba250a536840d3db
Magnar Sveen <magnars@gmail.com>
2012-04-20 14:10:35 +0200

    Nicer M-<up><down> in dired

--- setup-dired.el
+++ setup-dired.el
@@ -15,6 +15,26 @@
   (backward-char (- (current-column) 2)))
 
 (define-key dired-mode-map (kbd "C-a") 'dired-back-to-start-of-files)
+
+;; M-up is nicer in dired if it moves to the third line - straight to the ".."
+(defun dired-back-to-top ()
+  (interactive)
+  (beginning-of-buffer)
+  (next-line 2)
+  (dired-back-to-start-of-files))
+
+(define-key dired-mode-map (kbd "M-<up>") 'dired-back-to-top)
+
+;; M-down is nicer in dired if it moves to the last file
+(defun dired-jump-to-bottom ()
+  (interactive)
+  (end-of-buffer)
+  (next-line -1)
+  (dired-back-to-start-of-files))
+
+(define-key dired-mode-map (kbd "M-<down>") 'dired-jump-to-bottom)
+
+;; Delete with C-x C-k to match file buffers and magit
 (define-key dired-mode-map (kbd "C-x C-k") 'dired-do-delete)
 
 (eval-after-load "wdired"
