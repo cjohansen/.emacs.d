6e114f34252bf170a8ec4c2edeb2684acf66f536
Magnar Sveen <magnars@gmail.com>
2012-04-27 09:31:38 +0200

    ediff side by side

--- sane-defaults.el
+++ sane-defaults.el
@@ -85,6 +85,11 @@
 (require 'uniquify)
 (setq uniquify-buffer-name-style 'forward)
 
+;; A saner ediff
+(setq ediff-diff-options "-w")
+(setq ediff-split-window-function 'split-window-horizontally)
+(setq ediff-window-setup-function 'ediff-setup-windows-plain)
+
 ;; Easily navigate sillycased words
 (global-subword-mode 1)
 
