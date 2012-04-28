38b7654fc6410fcb7848221dfcfee18e7449d4c0
Magnar Sveen <magnars@gmail.com>
2012-04-28 23:07:05 +0200

    magit: toggle-ignore-whitespace with W

--- setup-magit.el
+++ setup-magit.el
@@ -1,12 +1,18 @@
 (require 'magit)
 (require 'magit-svn)
 
+;; C-x C-k to kill file on line
+
 (defun magit-kill-file-on-line ()
   "Show file on current magit line and prompt for deletion."
   (interactive)
   (magit-visit-item)
   (delete-current-buffer-file)
   (magit-refresh))
+
+(define-key magit-status-mode-map (kbd "C-x C-k") 'magit-kill-file-on-line)
+
+;; full screen magit-status
 
 (defadvice magit-status (around magit-fullscreen activate)
   (window-configuration-to-register :magit-fullscreen)
@@ -21,6 +27,25 @@ buffer"
   (jump-to-register :magit-fullscreen))
 
 (define-key magit-status-mode-map (kbd "q") 'magit-quit-session)
-(define-key magit-status-mode-map (kbd "C-x C-k") 'magit-kill-file-on-line)
+
+;; ignore whitespace
+
+(defun magit-toggle-whitespace ()
+  (interactive)
+  (if (member "-w" magit-diff-options)
+      (magit-dont-ignore-whitespace)
+    (magit-ignore-whitespace)))
+
+(defun magit-ignore-whitespace ()
+  (interactive)
+  (add-to-list 'magit-diff-options "-w")
+  (magit-refresh))
+
+(defun magit-dont-ignore-whitespace ()
+  (interactive)
+  (setq magit-diff-options (remove "-w" magit-diff-options))
+  (magit-refresh))
+
+(define-key magit-status-mode-map (kbd "W") 'magit-toggle-whitespace)
 
 (provide 'setup-magit)
