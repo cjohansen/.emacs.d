2c04026a1d34dd2523ab57e42cae9e3e3751248d
Magnar Sveen <magnars@gmail.com>
2012-05-14 07:41:13 +0200

    Add /*global */ declarations to local js2 externs.

--- setup-js2-mode.el
+++ setup-js2-mode.el
@@ -56,4 +56,20 @@
                                         (match-end 1) "\u2190")
                         nil)))))
 
+;; After js2 has parsed a js file, we look for jslint globals decl comment ("/* global Fred, _, Harry */") and
+;; add any symbols to a buffer-local var of acceptable global vars
+;; Note that we also support the "symbol: true" way of specifying names via a hack (remove any ":true"
+;; to make it look like a plain decl, and any ':false' are left behind so they'll effectively be ignored as
+;; you can;t have a symbol called "someName:false"
+(add-hook 'js2-post-parse-callbacks
+          (lambda ()
+            (let ((btext (replace-regexp-in-string
+                          ": *true" " "
+                          (replace-regexp-in-string "[\n\t ]+" " " (buffer-substring-no-properties 1 (buffer-size)) t t))))
+              (mapc (apply-partially 'add-to-list 'js2-additional-externs)
+                    (split-string
+                     (if (string-match "/\\* *global *\\(.*?\\) *\\*/" btext) (match-string-no-properties 1 btext) "")
+                     " *, *" t))
+              )))
+
 (provide 'setup-js2-mode)
