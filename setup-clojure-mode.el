e195e03ee6303e57d76d14c7619123404b3bcf35
Magnar Sveen <magnars@gmail.com>
2012-05-24 13:52:33 +0200

    Change clj keybindings for jumping between tests and source.

--- setup-clojure-mode.el
+++ setup-clojure-mode.el
@@ -1,6 +1,6 @@
 (require 'clojure-mode)
 
-(define-key clojure-mode-map (kbd "C-c RET jo") 'clj-jump-to-other-file)
-(define-key clojure-mode-map (kbd "C-c RET oo") 'clj-jump-to-other-file-other-window)
+(define-key clojure-mode-map (kbd "C-c C-j") 'clj-jump-to-other-file)
+(define-key clojure-mode-map (kbd "C-c M-j") 'clj-jump-to-other-file-other-window)
 
 (provide 'setup-clojure-mode)
