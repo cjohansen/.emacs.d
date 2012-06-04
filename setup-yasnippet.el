3ccdec78d37a5a8af3f80f0f5c235f981894bbeb
Magnar Sveen <magnars@gmail.com>
2012-06-04 20:25:21 +0200

    Only use own snippets - skip the ones bundled with yasnippet
     - they're old and not changing.

--- setup-yasnippet.el
+++ setup-yasnippet.el
@@ -1,13 +1,8 @@
-;; Load and initialize yasnippet
 (require 'yasnippet)
-(yas/initialize)
 
-;; Develop in ~/emacs.d/snippets, but also
-;; include snippets that come with yasnippet
-(setq yas/root-directory `(,(expand-file-name "snippets" dotfiles-dir)
-                           ,(expand-file-name "yasnippet/snippets" site-lisp-dir)))
-
-(mapc 'yas/load-directory yas/root-directory)
+;; Use only own snippets, do not use bundled ones
+(setq yas/snippet-dirs '("~/.emacs.d/snippets"))
+(yas/global-mode 1)
 
 ;; Include snippets for Buster.js
 (require 'buster-snippets)
