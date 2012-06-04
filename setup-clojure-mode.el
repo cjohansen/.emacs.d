c4535faadae97c2119e4d1766d6f28d39f28c470
Magnar Sveen <magnars@gmail.com>
2012-06-04 20:24:59 +0200

    Save buffer before running tests in clojure-test-mode

--- setup-clojure-mode.el
+++ setup-clojure-mode.el
@@ -3,4 +3,9 @@
 (define-key clojure-mode-map (kbd "C-c C-j") 'clj-jump-to-other-file)
 (define-key clojure-mode-map (kbd "C-c M-j") 'clj-jump-to-other-file-other-window)
 
+(defadvice clojure-test-run-tests (before save-first activate)
+  (save-buffer))
+
+(autoload 'clojure-test-mode "clojure-test-mode")
+
 (provide 'setup-clojure-mode)
