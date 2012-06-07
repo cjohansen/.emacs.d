cac379d1f07ffafe33cea63053f5a56dbc9dee4a
Magnar Sveen <magnars@gmail.com>
2012-06-07 11:28:07 +0200

    Update multiple-cursors
     - now with multiple regions too.

--- key-bindings.el
+++ key-bindings.el
@@ -16,7 +16,7 @@
 (global-set-key (if is-mac (kbd "C-@") (kbd "C-'")) 'er/expand-region)
 
 ;; Experimental multiple-cursors
-(global-set-key (kbd "C-S-c C-S-c") 'mc/add-multiple-cursors-to-region-lines)
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
 (global-set-key (kbd "C-S-c C-e") 'mc/edit-ends-of-lines)
 (global-set-key (kbd "C-S-c C-a") 'mc/edit-beginnings-of-lines)
 
