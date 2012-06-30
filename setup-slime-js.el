6601ceceaf2bfdaa2ef056f76dcf9b6d15b94ddc
Magnar Sveen <magnars@gmail.com>
2012-06-30 12:10:19 +0200

    Automatically refresh css when saving, if jacked-in.

--- setup-slime-js.el
+++ setup-slime-js.el
@@ -2,6 +2,7 @@
 (setq slime-js-swank-command "swank-js")
 (setq slime-js-swank-args '())
 (setq slime-js-browser-command "open -a Safari")
+(setq slime-js-browser-jacked-in nil)
 
 (defun slime-js-run-swank ()
   "Runs the swank side of the equation."
@@ -24,6 +25,11 @@
   (shell-command (concat slime-js-browser-command " http://localhost:8009"))
   (sleep-for 3)
   (setq slime-remote-history nil)
-  (slime-js-select-remote (caadr (slime-eval '(js:list-remotes)))))
+  (slime-js-select-remote (caadr (slime-eval '(js:list-remotes))))
+  (setq slime-js-browser-jacked-in t))
+
+(defadvice save-buffer (after save-css-buffer activate)
+  (when (and slime-js-browser-jacked-in (eq major-mode 'css-mode))
+    (slime-js-refresh-css)))
 
 (provide 'setup-slime-js)
