c9f6faaa96cd6bfb0d1a3b4fb7d7257db96b1e47
Magnar Sveen <magnars@gmail.com>
2012-06-30 13:01:08 +0200

    slime-js eval statement at point with C-x C-e

--- setup-slime-js.el
+++ setup-slime-js.el
@@ -8,17 +8,19 @@
   "Runs the swank side of the equation."
   (interactive)
   (unless (boundp 'slime-js-swank-buffer)
-          (setq slime-js-swank-buffer
-                (apply #'make-comint "swank-js"  slime-js-swank-command nil slime-js-swank-args))))
+    (setq slime-js-swank-buffer
+          (apply #'make-comint "swank-js"  slime-js-swank-command nil slime-js-swank-args))))
 
-(defun js2-jack-in-node ()
+(defun slime-js-jack-in-node ()
+  "Start a swank-js server and connect to it, opening a repl."
   (interactive)
   (slime-js-run-swank)
   (sleep-for 1)
   (setq slime-protocol-version 'ignore)
   (slime-connect "localhost" 4005))
 
-(defun js2-jack-in-browser ()
+(defun slime-js-jack-in-browser ()
+  "Start a swank-js server, connect to it, open a repl, open a browser, connect to that."
   (interactive)
   (js2-jack-in-node)
   (sleep-for 2)
@@ -32,5 +34,19 @@
 (defadvice save-buffer (after save-css-buffer activate)
   (when (and slime-js-browser-jacked-in (eq major-mode 'css-mode))
     (slime-js-refresh-css)))
+
+(defun js2-is-eval-friendly-node (n)
+  (or (and (js2-stmt-node-p n) (not (js2-block-node-p n)))
+      (and (js2-function-node-p n) (js2-function-node-name n))))
+
+(defun slime-js-eval-statement ()
+  (interactive)
+  (let ((stmt (js2r--closest 'js2-is-eval-friendly-node)))
+    (slime-flash-region (js2-node-abs-pos stmt) (js2-node-abs-end stmt))
+    (slime-js-eval
+     (js2-node-string stmt)
+     #'(lambda (s) (message (cadr s))))))
+
+(define-key slime-js-minor-mode-map (kbd "C-x C-e") 'slime-js-eval-statement)
 
 (provide 'setup-slime-js)
