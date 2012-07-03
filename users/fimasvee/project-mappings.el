118aa99243b71c3414cf6658548c7372b6e9cd0f
Magnar Sveen <magnars@gmail.com>
2012-07-03 10:43:20 +0200

    slime-js: Added ability to change connect url.

--- users/fimasvee/project-mappings.el
+++ users/fimasvee/project-mappings.el
@@ -44,6 +44,7 @@
 
 (project-specifics "oppdrag-services"
                    (set (make-local-variable 'slime-js-target-url) "http://local.finn.no:8080/")
+                   (set (make-local-variable 'slime-js-connect-url) "http://local.finn.no:8009")
                    (set (make-local-variable 'slime-js-starting-url) "/oppdrag/")
                    (ffip-local-patterns "*.js" "*.jsp" "*.css" "*.org" "*.vm" "*jsTestDriver.conf" "*jawr.properties")
                    (oppdrag-mode))
