666b983b619e087fc06cefbc5898fb9aa17dc428
Magnar Sveen <magnars@gmail.com>
2012-07-17 14:17:36 +0200

    Keep point position when duplicating stuff.

--- defuns/editing-defuns.el
+++ defuns/editing-defuns.el
@@ -44,9 +44,10 @@
   "Duplicates the current line or region ARG times.
 If there's no region, the current line will be duplicated."
   (interactive "p")
-  (if (region-active-p)
-      (duplicate-region arg)
-    (duplicate-current-line arg)))
+  (save-excursion
+    (if (region-active-p)
+        (duplicate-region arg)
+      (duplicate-current-line arg))))
 
 (defun duplicate-region (num &optional start end)
   "Duplicates the region bounded by START and END NUM times.
@@ -57,7 +58,7 @@ region-end is used. Adds the duplicated text to the kill ring."
          (end (or end (region-end)))
          (region (buffer-substring start end)))
     (kill-ring-save start end)
-    (goto-char end)
+    (goto-char start)
     (dotimes (i num)
       (insert region))))
 
@@ -68,8 +69,7 @@ region-end is used. Adds the duplicated text to the kill ring."
     (goto-char (point-max))
     (newline)
     (forward-char -1))
-  (duplicate-region num (point-at-bol) (1+ (point-at-eol)))
-  (goto-char (1- (point))))
+  (duplicate-region num (point-at-bol) (1+ (point-at-eol))))
 
 (defun yank-indented ()
   (interactive)
