b61607bc2b8b91fdd251ce3f09b10de07fa9f70b
Magnar Sveen <magnars@gmail.com>
2012-08-14 18:40:46 +0200

    Add browse-url-at-point to C-x M-g
     - similar to C-x g which is webjump

--- key-bindings.el
+++ key-bindings.el
@@ -141,6 +141,7 @@
 
 ;; Webjump let's you quickly search google, wikipedia, emacs wiki
 (global-set-key (kbd "C-x g") 'webjump)
+(global-set-key (kbd "C-x M-g") 'browse-url-at-point)
 
 ;; Completion at point
 (global-set-key (kbd "C-<tab>") 'completion-at-point)
