5bed77d2bb89e16c15cb2c387bb68c654caa548e
Magnar Sveen <magnars@gmail.com>
2012-08-16 15:57:15 +0200

    Diminish elisp-slime-nav modeline clutter.

--- init.el
+++ init.el
@@ -118,6 +118,7 @@
 ;; Elisp go-to-definition with M-. and back again with M-,
 (autoload 'elisp-slime-nav-mode "elisp-slime-nav")
 (add-hook 'emacs-lisp-mode-hook (lambda () (elisp-slime-nav-mode t)))
+(eval-after-load 'elisp-slime-nav '(diminish 'elisp-slime-nav-mode))
 
 ;; Emacs server
 (require 'server)
