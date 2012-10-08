d0761f9240eb3a446b479d8b0757c604ad1a3ce9
Magnar Sveen <magnars@gmail.com>
2012-10-08 11:24:50 +0200

    Use C-, to complete command in nrepl-modes.

--- setup-clojure-mode.el
+++ setup-clojure-mode.el
@@ -6,4 +6,9 @@
 (defadvice clojure-test-run-tests (before save-first activate)
   (save-buffer))
 
+(eval-after-load "nrepl"
+  '(progn
+     (define-key nrepl-mode-map (kbd "C-,") 'complete-symbol)
+     (define-key nrepl-interaction-mode-map (kbd "C-,") 'complete-symbol)))
+
 (provide 'setup-clojure-mode)
