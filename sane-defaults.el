434694612779e1894ce45ad5575f511aa1cb15d1
Magnar Sveen <magnars@gmail.com>
2012-10-08 11:24:31 +0200

    Skip past expand-region cruft when popping mark after a copy.

--- sane-defaults.el
+++ sane-defaults.el
@@ -105,8 +105,13 @@
 (setq vc-make-backup-files t)
 
 ;; When popping the mark, continue popping until the cursor actually moves
+;; Also, if the last command was a copy - skip past all the expand-region cruft.
 (defadvice pop-to-mark-command (around ensure-new-position activate)
   (let ((p (point)))
+    (when (eq last-command 'save-region-or-current-line)
+      ad-do-it
+      ad-do-it
+      ad-do-it)
     (dotimes (i 10)
       (when (= p (point)) ad-do-it))))
 
