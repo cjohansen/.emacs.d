7f7ca300941180cb717e35b89485e36e916105fe
Magnar Sveen <magnars@gmail.com>
2012-10-08 11:25:12 +0200

    Use `e` as key to enable wgrep (same as occur)

--- setup-rgrep.el
+++ setup-rgrep.el
@@ -43,6 +43,9 @@
      ;; Add custom keybindings
      (define-key grep-mode-map "q" 'rgrep-quit-window)
      (define-key grep-mode-map (kbd "C-<return>") 'rgrep-goto-file-and-close-rgrep)
-     (define-key grep-mode-map (kbd "C-x C-s") 'wgrep-save-all-buffers)))
+     (define-key grep-mode-map (kbd "C-x C-s") 'wgrep-save-all-buffers)
+
+     ;; Use same keybinding as occur
+     (setq wgrep-enable-key "e")))
 
 (provide 'setup-rgrep)
