ff8ee9511e4abe2f54454f3a15b148893abbcd5f
Magnar Sveen <magnars@gmail.com>
2012-10-17 08:06:19 +0200

    Open capfile in ruby-mode

--- mode-mappings.el
+++ mode-mappings.el
@@ -42,6 +42,7 @@
 (add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.ru$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("capfile" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.erb$" . rhtml-mode))
 
 (eval-after-load 'ruby-mode '(require 'setup-ruby-mode))
