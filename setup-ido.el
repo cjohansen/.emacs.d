9e4a4d57d1f1e0e1b5fbec24ffceb72ee519b799
Magnar Sveen <magnars@gmail.com>
2012-10-19 16:13:50 +0200

    ido: be case sensitive, and don't match other directories.

--- setup-ido.el
+++ setup-ido.el
@@ -4,6 +4,8 @@
 (ido-mode t)
 (setq ido-enable-prefix nil
       ido-enable-flex-matching t
+      ido-case-fold nil
+      ido-auto-merge-work-directories-length -1
       ido-create-new-buffer 'always
       ido-use-filename-at-point nil
       ido-max-prospects 10)
