fd3d739f20f1e12f37c2f308ee900678ba8c38ed
Magnar Sveen <magnars@gmail.com>
2012-10-23 21:55:23 +0200

    Add some more paredit-goodies.

--- setup-paredit.el
+++ setup-paredit.el
@@ -2,10 +2,19 @@
 
 (require 'paredit)
 
+(defun paredit-wrap-round-from-behind ()
+  (interactive)
+  (forward-sexp -1)
+  (paredit-wrap-round)
+  (insert " ")
+  (forward-char -1))
+
 (defun setup-paredit-for-mode-map (mode-map)
   (define-key mode-map (kbd "s-<up>") 'paredit-raise-sexp)
   (define-key mode-map (kbd "s-<right>") 'paredit-forward-slurp-sexp)
   (define-key mode-map (kbd "s-8") 'paredit-wrap-round)
+  (define-key mode-map (kbd "s-9") 'paredit-wrap-round-from-behind)
+  (define-key mode-map (kbd "s-<backspace>") 'paredit-splice-sexp-killing-backward)
   (define-key mode-map (kbd "s-t") 'transpose-sexps))
 
 (eval-after-load "lisp-mode" '(setup-paredit-for-mode-map emacs-lisp-mode-map))
