ee33259b76cac3dd965eeafc9f8565fddea61458
Magnar Sveen <magnars@gmail.com>
2012-12-02 08:51:44 +0100

    Add html-wrap-in-tag at C-c C-w

--- setup-html-mode.el
+++ setup-html-mode.el
@@ -1,9 +1,3 @@
-(eval-after-load "sgml-mode"
-  '(progn
-
-     (define-key html-mode-map (kbd "C-<down>") 'skip-to-next-blank-line)
-     (define-key html-mode-map (kbd "C-<up>") 'skip-to-previous-blank-line)))
-
 (defun skip-to-next-blank-line ()
   (interactive)
   (let ((inhibit-changing-match-data t))
@@ -17,6 +11,25 @@
     (forward-char -1)
     (unless (search-backward-regexp "^\\s *$" nil t)
       (forward-char 1))))
+
+(defun html-wrap-in-tag (beg end)
+  (interactive "r")
+  (let ((oneline? (= (line-number-at-pos beg) (line-number-at-pos end))))
+    (deactivate-mark)
+    (goto-char end)
+    (unless oneline? (newline-and-indent))
+    (insert "</div>")
+    (goto-char beg)
+    (insert "<div>")
+    (unless oneline? (newline-and-indent))
+    (indent-region beg (+ end 11))
+    (goto-char (+ beg 4))))
+
+(eval-after-load "sgml-mode"
+  '(progn
+     (define-key html-mode-map (kbd "C-<down>") 'skip-to-next-blank-line)
+     (define-key html-mode-map (kbd "C-<up>") 'skip-to-previous-blank-line)
+     (define-key html-mode-map (kbd "C-c C-w") 'html-wrap-in-tag)))
 
 (autoload 'zencoding-mode "zencoding-mode")
 (autoload 'zencoding-expand-line "zencoding-mode")
