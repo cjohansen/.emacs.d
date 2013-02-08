a3683fc5a81e44649f352cd7192ca0e92828f9fe
Magnar Sveen <magnars@gmail.com>
2013-02-08 20:22:32 +0100

    Go straight home with ~ in ido-find-file

--- setup-ido.el
+++ setup-ido.el
@@ -13,11 +13,20 @@
 (add-hook
  'ido-setup-hook
  (lambda ()
+   ;; Go straight home
+   (define-key ido-file-completion-map
+     (kbd "~")
+     (lambda ()
+       (interactive)
+       (cond
+        ((looking-back "~/") (insert "projects/"))
+        ((looking-back "/") (insert "~/"))
+        (:else (call-interactively 'self-insert-command)))))
 
-     ;; Use C-w to go back up a dir to better match normal usage of C-w
-     ;; - insert current file name with C-x C-w instead.
-     (define-key ido-file-completion-map (kbd "C-w") 'ido-delete-backward-updir)
-     (define-key ido-file-completion-map (kbd "C-x C-w") 'ido-copy-current-file-name)))
+   ;; Use C-w to go back up a dir to better match normal usage of C-w
+   ;; - insert current file name with C-x C-w instead.
+   (define-key ido-file-completion-map (kbd "C-w") 'ido-delete-backward-updir)
+   (define-key ido-file-completion-map (kbd "C-x C-w") 'ido-copy-current-file-name)))
 
 ;; Always rescan buffer for imenu
 (set-default 'imenu-auto-rescan t)
