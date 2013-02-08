0c1f007492bcb118dd4ab5020a743bbaeeada765
Magnar Sveen <magnars@gmail.com>
2013-02-08 20:17:18 +0100

    More commands for multiple-cursors lists.

--- .mc-lists.el
+++ .mc-lists.el
@@ -5,6 +5,7 @@
       '(
         ac-complete
         back-to-indentation-or-beginning
+        backward-sexp
         c-electric-backspace
         c-electric-delete-forward
         c-electric-paren
@@ -18,42 +19,79 @@
         delete-blank-lines
         dired-back-to-start-of-files
         duplicate-current-line-or-region
+        end-of-buffer
         eval-and-replace
         fold-this
         forward-paragraph
+        forward-sentence
+        forward-sexp
+        hippie-expand-no-case-fold
         html-wrap-in-tag
         js2-beginning-of-line
         js2-end-of-line
         js2-insert-and-indent
         js2r-inline-var
         js2r-var-to-this
+        kill-and-retry-line
         kill-region-or-backward-word
         magit-stage-item
         markdown-enter-key
         move-line-down
         move-line-up
+        move-text-down
+        move-text-up
+        move-to-window-line-top-bottom
         new-line-above
+        open-line-above
+        open-line-below
         org-beginning-of-line
         org-delete-backward-char
         org-delete-char
         org-end-of-line
         org-metaleft
         org-metaright
+        org-return-indent
         org-self-insert-command
         org-shiftright
         orgtbl-self-insert-command
+        paredit-backward
+        paredit-doublequote
+        paredit-forward-delete
+        paredit-forward-kill-word
+        paredit-forward-slurp-sexp
+        paredit-forward-up
+        paredit-join-sexps
+        paredit-kill
+        paredit-kill-region-or-backward-word
+        paredit-newline
+        paredit-open-curly
+        paredit-open-round
+        paredit-open-square
+        paredit-raise-sexp
+        paredit-semicolon
+        paredit-splice-sexp
+        paredit-wrap-round
+        paredit-wrap-square
         quoted-insert
         save-region-or-current-line
         sgml-delete-tag
         sgml-slash
+        skeleton-pair-insert-maybe
         slime-space
+        sp-self-insert-command
+        tagedit-kill-attribute
+        transpose-sexps
         wrap-region-trigger
         yank-indented
         yas/expand
+        zap-up-to-char
         ))
 
 (setq mc/cmds-to-run-once
       '(
+        dired-toggle-read-only
+        goto-line-with-feedback
+        ido-switch-buffer
         js2r-extract-var
         js2r-rename-var
         smex
