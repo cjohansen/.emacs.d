3a6a7b798cf60ac7e2996a364b7bc5bfb51ab8c8
Magnar Sveen <magnars@gmail.com>
2013-02-10 21:58:20 +0100

    Always use eldoc-mode

--- init.el
+++ init.el
@@ -150,7 +150,7 @@
 (diminish 'yas/minor-mode)
 ;; Elisp go-to-definition with M-. and back again with M-,
 (autoload 'elisp-slime-nav-mode "elisp-slime-nav")
-(add-hook 'emacs-lisp-mode-hook (lambda () (elisp-slime-nav-mode t)))
+(add-hook 'emacs-lisp-mode-hook (lambda () (elisp-slime-nav-mode t) (eldoc-mode 1)))
 (eval-after-load 'elisp-slime-nav '(diminish 'elisp-slime-nav-mode))
 
 ;; Email, baby
