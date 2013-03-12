9d663a8f526b3cfa54bc9bd4ef7bf13a2e0984c1
Magnar Sveen <magnars@gmail.com>
2013-03-12 21:36:49 +0100

    Personal: update notat.adv -> org

--- users/fimasvee/project-mappings.el
+++ users/fimasvee/project-mappings.el
@@ -200,7 +200,7 @@
 (defun custom-persp/adventur-master ()
   (interactive)
   (custom-persp "adventur-master"
-                (find-file "~/projects/eventyr/master/notat.adv")))
+                (find-file "~/projects/eventyr/master/notat.org")))
 
 (define-key persp-mode-map (kbd "C-x p m") 'custom-persp/adventur-master)
 
