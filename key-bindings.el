b723e48125b3c91e89f64223b6c086a1dc471148
Magnar Sveen <magnars@gmail.com>
2013-04-15 08:36:30 +0200

    Add completion-at-point to C-, globally

--- key-bindings.el
+++ key-bindings.el
@@ -6,6 +6,7 @@
 ;; Completion that uses many different methods to find options.
 (global-set-key (kbd "C-.") 'hippie-expand-no-case-fold)
 (global-set-key (kbd "C-:") 'hippie-expand-lines)
+(global-set-key (kbd "C-,") 'completion-at-point)
 
 (require 'misc)
 (global-set-key (kbd "s-.") 'copy-from-above-command)
