19b1d6b8797ddfc65d822f4d3d20c7a50100cd73
Magnar Sveen <magnars@gmail.com>
2013-04-21 02:16:15 +0200

    Enable `paredit-mode' in the minibuffer, during `eval-expression'.
     - thanks to @bbatsov :-)

--- setup-paredit.el
+++ setup-paredit.el
@@ -62,6 +62,13 @@
 ;; don't hijack \ please
 (define-key paredit-mode-map (kbd "\\") nil)
 
+;; Enable `paredit-mode' in the minibuffer, during `eval-expression'.
+(defun conditionally-enable-paredit-mode ()
+  (if (eq this-command 'eval-expression)
+      (paredit-mode 1)))
+
+(add-hook 'minibuffer-setup-hook 'conditionally-enable-paredit-mode)
+
 ;; making paredit work with delete-selection-mode
 (put 'paredit-forward-delete 'delete-selection 'supersede)
 (put 'paredit-backward-delete 'delete-selection 'supersede)
