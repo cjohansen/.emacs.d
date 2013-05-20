962f8b2662821f5917001c06af3fa4984a7ce755
Magnar Sveen <magnars@gmail.com>
2013-05-20 11:55:21 +0200

    Personal: turn off mouse-wheel-mode only in window-system

--- users/fimasvee/init.el
+++ users/fimasvee/init.el
@@ -3,7 +3,8 @@
 (setq js-indent-level 2)
 
 ;; Disallow scrolling with mouse wheel
-(mouse-wheel-mode -1)
+(when window-system
+  (mouse-wheel-mode -1))
 
 ;; Font size
 (define-key global-map (kbd "M-s +") 'zoom-in)
