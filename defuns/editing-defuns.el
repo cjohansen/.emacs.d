3678f7b38b7dcbafc4166ca9dab006a1370d64ab
Magnar Sveen <magnars@gmail.com>
2013-06-06 19:46:20 +0200

    Support for tags in newline-dwim

--- defuns/editing-defuns.el
+++ defuns/editing-defuns.el
@@ -24,6 +24,7 @@
 (defun new-line-dwim ()
   (interactive)
   (let ((break-open-pair (or (and (looking-back "{") (looking-at "}"))
+                             (and (looking-back ">") (looking-at "<"))
                              (and (looking-back "\\[") (looking-at "\\]")))))
     (newline)
     (when break-open-pair
