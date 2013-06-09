0e2d2a6c5daeae6849af858eeee9ccfa22d24f8d
Magnar Sveen <magnars@gmail.com>
2013-06-09 22:36:27 +0200

    Use js-mode for .jshintrc files

--- mode-mappings.el
+++ mode-mappings.el
@@ -70,6 +70,7 @@
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.json$" . javascript-mode))
+(add-to-list 'auto-mode-alist '("\\.jshintrc$" . javascript-mode))
 (add-to-list 'magic-mode-alist '("#!/usr/bin/env node" . js2-mode))
 
 ;; Configuration files
