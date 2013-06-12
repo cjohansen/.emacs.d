08ed1237384fc11943b16ee15d1d8eb64e4c222c
Magnar Sveen <magnars@gmail.com>
2013-06-12 09:52:34 +0200

    Add @purcell's require-package

--- setup-package.el
+++ setup-package.el
@@ -15,4 +15,18 @@
       (package-install it)))
   (delete-other-windows))
 
+;;; On-demand installation of packages
+
+(defun require-package (package &optional min-version no-refresh)
+  "Install given PACKAGE, optionally requiring MIN-VERSION.
+If NO-REFRESH is non-nil, the available package lists will not be
+re-downloaded in order to locate PACKAGE."
+  (if (package-installed-p package min-version)
+      t
+    (if (or (assoc package package-archive-contents) no-refresh)
+        (package-install package)
+      (progn
+        (package-refresh-contents)
+        (require-package package min-version t)))))
+
 (provide 'setup-package)
