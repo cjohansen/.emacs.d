d7ab6e12e4e239ec355dad6562635298bd9830da
Magnar Sveen <magnars@gmail.com>
2013-06-24 22:58:23 +0200

    Include more of path with find-recent-files

--- defuns/buffer-defuns.el
+++ defuns/buffer-defuns.el
@@ -152,8 +152,15 @@ Including indent-buffer, which should not be called automatically on save."
   (concat (cadr (reverse (split-string file-name "/"))) "/"
           (file-name-nondirectory file-name)))
 
+(require 's)
+
+(defvar user-home-directory (concat (expand-file-name "~") "/"))
+
+(defun shorter-file-name (file-name)
+  (s-chop-prefix user-home-directory file-name))
+
 (defun recentf--file-cons (file-name)
-  (cons (file-name-with-one-directory file-name) file-name))
+  (cons (shorter-file-name file-name) file-name))
 
 (defun recentf-ido-find-file ()
   "Find a recent file using ido."
