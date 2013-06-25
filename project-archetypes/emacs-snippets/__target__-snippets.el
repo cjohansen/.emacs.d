c716781f5f8f943d66aec15275b21ea8c011a3dd
Magnar Sveen <magnars@gmail.com>
2013-06-25 09:14:26 +0200

    Add yasnippet dependency to emacs-snippets archetype

--- project-archetypes/emacs-snippets/__target__-snippets.el
+++ project-archetypes/emacs-snippets/__target__-snippets.el
@@ -5,7 +5,7 @@
 ;; Author: Magnar Sveen <magnars@gmail.com>
 ;; Keywords: snippets
 ;; Version: 0.1.0
-;; Package-Requires: ((s "1.4.0") (dash "1.2.0"))
+;; Package-Requires: ((s "1.4.0") (dash "1.2.0") (yasnippet "0.6.1"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
