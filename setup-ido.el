f34be4628cab040dae4f309c2042e21c07146c37
Magnar Sveen <magnars@gmail.com>
2013-07-18 21:52:59 +0200

    Fix C-w in ido-read-directory-name

--- setup-ido.el
+++ setup-ido.el
@@ -12,7 +12,6 @@
 
 ;; Try out flx-ido for better flex matching between words
 (require 'flx-ido)
-(ido-everywhere 1)
 (flx-ido-mode 1)
 ;; disable ido faces to see flx highlights.
 (setq ido-use-faces nil)
@@ -27,23 +26,28 @@
   (define-key ido-completion-map (kbd "C-p") 'ido-prev-match)
   (define-key ido-completion-map (kbd "<up>") 'ido-prev-match))
 
-(add-hook
- 'ido-setup-hook
- (lambda ()
-   ;; Go straight home
-   (define-key ido-file-completion-map
-     (kbd "~")
-     (lambda ()
-       (interactive)
-       (cond
-        ((looking-back "~/") (insert "projects/"))
-        ((looking-back "/") (insert "~/"))
-        (:else (call-interactively 'self-insert-command)))))
+(require 'dash)
 
-   ;; Use C-w to go back up a dir to better match normal usage of C-w
-   ;; - insert current file name with C-x C-w instead.
-   (define-key ido-file-completion-map (kbd "C-w") 'ido-delete-backward-updir)
-   (define-key ido-file-completion-map (kbd "C-x C-w") 'ido-copy-current-file-name)))
+(defun my/setup-ido ()
+  ;; Go straight home
+  (define-key ido-file-completion-map
+    (kbd "~")
+    (lambda ()
+      (interactive)
+      (cond
+       ((looking-back "~/") (insert "projects/"))
+       ((looking-back "/") (insert "~/"))
+       (:else (call-interactively 'self-insert-command)))))
+
+  ;; Use C-w to go back up a dir to better match normal usage of C-w
+  ;; - insert current file name with C-x C-w instead.
+  (define-key ido-file-completion-map (kbd "C-w") 'ido-delete-backward-updir)
+  (define-key ido-file-completion-map (kbd "C-x C-w") 'ido-copy-current-file-name)
+
+  (define-key ido-file-dir-completion-map (kbd "C-w") 'ido-delete-backward-updir)
+  (define-key ido-file-dir-completion-map (kbd "C-x C-w") 'ido-copy-current-file-name))
+
+(add-hook 'ido-setup-hook 'my/setup-ido)
 
 ;; Always rescan buffer for imenu
 (set-default 'imenu-auto-rescan t)
