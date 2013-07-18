ddd9c482cc63ccfebc8dc92e1f3f43d8281750ea
Magnar Sveen <magnars@gmail.com>
2013-07-18 21:51:56 +0200

    Personal: run deftest at point

--- users/fimasvee/my-defuns.el
+++ users/fimasvee/my-defuns.el
@@ -56,4 +56,19 @@
                  (js2-node-abs-end node))
   (js2-node-short-name node))
 
+;; run clojure.test at point, with fixture (hardcoded I know)
+
+(defun run-deftest-at-point ()
+  (interactive)
+  (save-excursion
+    (search-backward "deftest")
+    (search-forward "(")
+    (backward-char)
+    (let* ((beg (point))
+           (end (forward-list))
+           (contents (buffer-substring-no-properties beg end)))
+      (nrepl-find-and-clear-repl-buffer)
+      (nrepl-interactive-eval
+       (concat "(h/reset-state-fixture (fn [] " contents "))")))))
+
 (provide 'my-defuns)
