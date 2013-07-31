6a5f9649c140bf922ddebf3848598101c3190f78
Magnar Sveen <magnars@gmail.com>
2013-07-31 12:13:30 +0200

    Remove outdated hook

--- mode-mappings.el
+++ mode-mappings.el
@@ -30,10 +30,6 @@
 (add-to-list 'auto-mode-alist '("\\.html\\'" . html-mode))
 (add-to-list 'auto-mode-alist '("\\.tag$" . html-mode))
 (add-to-list 'auto-mode-alist '("\\.vm$" . html-mode))
-(add-hook 'sgml-mode-hook
-          (lambda ()
-            (require 'rename-sgml-tag)
-            (define-key sgml-mode-map (kbd "C-c C-r") 'rename-sgml-tag)))
 
 ;; JSP
 (autoload 'crappy-jsp-mode "crappy-jsp-mode")
