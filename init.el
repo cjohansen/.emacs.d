9099d463661975960bba4b79b5122257b7d3608a
Magnar Sveen <magnars@gmail.com>
2013-09-27 19:12:29 +0200

    Don't highlight bookmark commands specifically

--- init.el
+++ init.el
@@ -98,7 +98,6 @@
 (require 'guide-key)
 (setq guide-key/guide-key-sequence '("C-x r" "C-x 4" "C-x v" "C-x 8"))
 (guide-key-mode 1)
-(setq guide-key/highlight-command-regexp "bookmark")
 (setq guide-key/recursive-key-sequence-flag t)
 (setq guide-key/popup-window-position 'bottom)
 
