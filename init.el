d4de0d7042489e867341d06fcc64f5368a078d31
Magnar Sveen <magnars@gmail.com>
2013-11-06 11:57:37 +0100

    Add guide-key for C-x +

--- init.el
+++ init.el
@@ -98,7 +98,7 @@
 
 ;; guide-key
 (require 'guide-key)
-(setq guide-key/guide-key-sequence '("C-x r" "C-x 4" "C-x v" "C-x 8"))
+(setq guide-key/guide-key-sequence '("C-x r" "C-x 4" "C-x v" "C-x 8" "C-x +"))
 (guide-key-mode 1)
 (setq guide-key/recursive-key-sequence-flag t)
 (setq guide-key/popup-window-position 'bottom)
