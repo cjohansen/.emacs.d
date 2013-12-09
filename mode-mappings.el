608c39c9ed4bad8f89fc466c36dd252be2ea49b9
Magnar Sveen <magnars@gmail.com>
2013-12-09 19:46:12 +0100

    Edit .gradle in groovy-mode

--- mode-mappings.el
+++ mode-mappings.el
@@ -56,6 +56,7 @@
 ;; Groovy
 (autoload 'groovy-mode "groovy-mode")
 (add-to-list 'auto-mode-alist '("\\.groovy$" . groovy-mode))
+(add-to-list 'auto-mode-alist '("\\.gradle$" . groovy-mode))
 
 ;; Clojure
 (autoload 'clojure-mode "clojure-mode")
