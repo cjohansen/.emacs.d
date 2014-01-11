ce1c6caef76d52370a8c3de75788a4073a1736d4
Magnar Sveen <magnars@gmail.com>
2014-01-11 18:59:42 +0100

    Better colors for ediff

--- setup-magit.el
+++ setup-magit.el
@@ -8,6 +8,18 @@
 (set-default 'magit-stage-all-confirm nil)
 (set-default 'magit-unstage-all-confirm nil)
 
+(eval-after-load 'ediff
+  '(progn
+     (set-face-foreground 'ediff-odd-diff-B "#ffffff")
+     (set-face-background 'ediff-odd-diff-B "#292521")
+     (set-face-foreground 'ediff-even-diff-B "#ffffff")
+     (set-face-background 'ediff-even-diff-B "#292527")
+
+     (set-face-foreground 'ediff-odd-diff-A "#ffffff")
+     (set-face-background 'ediff-odd-diff-A "#292521")
+     (set-face-foreground 'ediff-even-diff-A "#ffffff")
+     (set-face-background 'ediff-even-diff-A "#292527")))
+
 ;; todo:
 ;; diff-added-face      diff-changed-face
 ;; diff-context-face    diff-file-header-face
