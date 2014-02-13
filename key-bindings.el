906f423241c1eb27593315ce0286118f8ec9e046
Christian Johansen <christian@cjohansen.no>
2014-02-13 09:45:29 +0100

    Easy contract region

--- key-bindings.el
+++ key-bindings.el
@@ -21,6 +21,7 @@
 
 ;; Expand region (increases selected region by semantic units)
 (global-set-key (if is-mac (kbd "C-@") (kbd "C-'")) 'er/expand-region)
+(global-set-key (kbd "C-*") 'er/contract-region)
 
 ;; Experimental multiple-cursors
 (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
