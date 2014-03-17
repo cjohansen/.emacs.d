1bc3e8b4675e9d3e1c67f33036849fb7a6c08320
Magnar Sveen <magnars@gmail.com>
2014-03-17 08:51:54 +0100

    Use crappy-jsp-mode for html, since it has better indentation

--- mode-mappings.el
+++ mode-mappings.el
@@ -30,7 +30,7 @@
 (add-to-list 'auto-mode-alist '("\\.jade$" . jade-mode))
 
 ;; HTML
-(add-to-list 'auto-mode-alist '("\\.html\\'" . html-mode))
+(add-to-list 'auto-mode-alist '("\\.html\\'" . crappy-jsp-mode))
 (add-to-list 'auto-mode-alist '("\\.tag$" . html-mode))
 (add-to-list 'auto-mode-alist '("\\.vm$" . html-mode))
 
