ff7cd3caa1e9538fff769973768058d2fc085282
Christian Johansen <christian.johansen@nrk.no>
2014-12-17 12:59:44 +0100

    Save lots of stuff on the desktop

--- settings/setup-desktop.el
+++ settings/setup-desktop.el
@@ -3,7 +3,22 @@
 
 (desktop-save-mode 1)
 (setq history-length 250)
+
+(add-to-list 'desktop-globals-to-save 'read-expression-history)
+(add-to-list 'desktop-globals-to-save 'extended-command-history)
+(add-to-list 'desktop-globals-to-save 'find-tag-history)
+(add-to-list 'desktop-globals-to-save 'query-replace-history)
+(add-to-list 'desktop-globals-to-save 'grep-history)
 (add-to-list 'desktop-globals-to-save 'file-name-history)
+(add-to-list 'desktop-globals-to-save 'compile-history)
+(add-to-list 'desktop-globals-to-save 'kill-ring)
+(add-to-list 'desktop-globals-to-save 'replace-string-history)
+(add-to-list 'desktop-globals-to-save 'replace-regex-history)
+(add-to-list 'desktop-globals-to-save 'query-replace-regex-history)
+(add-to-list 'desktop-globals-to-save 'minibuffer-history)
+(add-to-list 'desktop-globals-to-save 'shell-command-history)
+(add-to-list 'desktop-globals-to-save 'buffer-name-history)
+(add-to-list 'desktop-globals-to-save 'find-file-history)
 
 (defun my-desktop-save ()
   (interactive)
