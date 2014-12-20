2ed049a7df93de3a738d019bcc54ce6d57b4ab5a
Magnar Sveen <magnars@gmail.com>
2014-12-20 23:12:27 +0100

    Use new yasnippet functions

--- settings/setup-yasnippet.el
+++ settings/setup-yasnippet.el
@@ -10,7 +10,7 @@
 (require 'datomic-snippets)
 
 ;; Jump to end of snippet definition
-(define-key yas-keymap (kbd "<return>") 'yas/exit-all-snippets)
+(define-key yas-keymap (kbd "<return>") 'yas-exit-all-snippets)
 
 ;; Inter-field navigation
 (defun yas/goto-end-of-active-field ()
@@ -33,7 +33,7 @@
 (define-key yas-keymap (kbd "C-a") 'yas/goto-start-of-active-field)
 
 ;; No dropdowns please, yas
-(setq yas-prompt-functions '(yas/ido-prompt yas/completing-prompt))
+(setq yas-prompt-functions '(yas-ido-prompt yas-completing-prompt))
 
 ;; No need to be so verbose
 (setq yas-verbosity 1)
