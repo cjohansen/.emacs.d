2a1559ef77e99d1ad8bfc746f4ac5e0df61fa990
Magnar Sveen <magnars@gmail.com>
2015-02-18 21:53:21 +0100

    Show .ejs files in html-mode

--- settings/mode-mappings.el
+++ settings/mode-mappings.el
@@ -33,6 +33,7 @@
 (add-to-list 'auto-mode-alist '("\\.html\\'" . crappy-jsp-mode))
 (add-to-list 'auto-mode-alist '("\\.tag$" . html-mode))
 (add-to-list 'auto-mode-alist '("\\.vm$" . html-mode))
+(add-to-list 'auto-mode-alist '("\\.ejs$" . html-mode))
 
 ;; JSP
 (autoload 'crappy-jsp-mode "crappy-jsp-mode")
