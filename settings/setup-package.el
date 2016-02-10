4b96e587ac559e6cf818315c01a414400177b802
Magnar Sveen <magnars@gmail.com>
2016-02-10 09:47:57 +0100

    Use stable cider and clojure-mode

--- settings/setup-package.el
+++ settings/setup-package.el
@@ -5,6 +5,10 @@
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 
+(setq package-pinned-packages
+      '((cider              . "melpa-stable")
+        (clojure-mode       . "melpa-stable")))
+
 (package-initialize)
 
 (unless (file-exists-p "~/.emacs.d/elpa/archives/melpa")
