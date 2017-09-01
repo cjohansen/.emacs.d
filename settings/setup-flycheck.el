4aab4a496c3faa9fdb714c350c62955d6164cd03
Magnar Sveen <magnars@gmail.com>
2017-09-01 11:25:30 +0200

    Remove outdated flycheck override
    See flycheck/flycheck#1305

--- settings/setup-flycheck.el
+++ settings/setup-flycheck.el
@@ -26,15 +26,4 @@ clean buffer we're an order of magnitude laxer about checking."
   '(custom-set-variables
     '(flycheck-display-errors-function #'flycheck-pos-tip-error-messages)))
 
-(defun flycheck-handle-idle-change ()
-  "Handle an expired idle time since the last change.
-
-This is an overwritten version of the original
-flycheck-handle-idle-change, which removes the forced deferred.
-Timers should only trigger inbetween commands in a single
-threaded system and the forced deferred makes errors never show
-up before you execute another command."
-  (flycheck-clear-idle-change-timer)
-  (flycheck-buffer-automatically 'idle-change))
-
 (provide 'setup-flycheck)
