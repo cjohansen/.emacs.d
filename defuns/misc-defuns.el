ab71a0bde080d2146fe0fc7c8c03a835775e8a41
Magnar Sveen <magnars@gmail.com>
2017-09-28 15:53:26 +0200

    Add fn to check face at point

--- defuns/misc-defuns.el
+++ defuns/misc-defuns.el
@@ -183,3 +183,9 @@ Both PATTERN and CONTENTS are matched as regular expressions."
   (edit-kbd-macro 'view-lossage))
 
 (defmacro comment (&rest ignore))
+
+(defun what-face (pos)
+  (interactive "d")
+  (let ((face (or (get-char-property pos 'read-face-name)
+                  (get-char-property pos 'face))))
+    (if face (message "Face: %s" face) (message "No face at %d" pos))))
