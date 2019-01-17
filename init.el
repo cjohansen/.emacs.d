19acc19a1e84122eea8da20ca2b4bbc943196413
Magnar Sveen <magnars@gmail.com>
2019-01-17 11:19:43 +0100

    Move emacs autosaves to temp, skip the lockfile behavior.
    These were culprits in re-running tests all the time, even when I had not yet
    saved my files.

--- init.el
+++ init.el
@@ -46,6 +46,13 @@
       `(("." . ,(expand-file-name
                  (concat user-emacs-directory "backups")))))
 
+;; Write all autosave files in the tmp dir
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
+
+;; Don't write lock-files, I'm the only one here
+(setq create-lockfiles nil)
+
 ;; Make backups of files, even when they're in version control
 (setq vc-make-backup-files t)
 
