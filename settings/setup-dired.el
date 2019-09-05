ed77fbffc0449a5e1cde483e3189f2b0c10c5e0c
Magnar Sveen <magnars@gmail.com>
2019-09-05 10:41:36 +0200

    Remove useless code
    - dired already does this now

--- settings/setup-dired.el
+++ settings/setup-dired.el
@@ -8,14 +8,6 @@
 ;; Move files between split panes
 (setq dired-dwim-target t)
 
-;; Reload dired after making changes
-(--each '(dired-do-rename
-          dired-do-copy
-          dired-create-directory
-          wdired-abort-changes)
-        (eval `(defadvice ,it (after revert-buffer activate)
-                 (revert-buffer))))
-
 ;; C-a is nicer in dired if it moves back to start of files
 (defun dired-back-to-start-of-files ()
   (interactive)
