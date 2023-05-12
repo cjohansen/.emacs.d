3421c88750ae9f9292ee999ad9045edf2060216a
Magnar Sveen <magnars@gmail.com>
2023-05-12 13:25:05 +0200

    Don't pester me with doccheck for my init.el and other emacs settings

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((emacs-lisp-mode . ((flycheck-disabled-checkers . (emacs-lisp-checkdoc)))))
